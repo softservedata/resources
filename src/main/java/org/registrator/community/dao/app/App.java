469774bdd2946e5080d1a75357d4b86eae0809e1
Achyp14 <achyp@MacBook-Pro-3.local>
2015-11-30 17:04:37 +0200

    Add method getAllUsers and Override toString() in UserDTO, PassportDTO, AddressDTO

--- src/main/java/org/registrator/community/dao/app/App.java
+++ src/main/java/org/registrator/community/dao/app/App.java
@@ -95,86 +95,86 @@ public class App {
 		
 		// Save new Resource in Database******
 
-		Tome tome = new Tome();
-		tome.setIdentifier("12345");
-		tome.setName("name??");
-		tome.setUser(DaoFactory.get().getUserDao().findById(1));
-		DaoFactory.get().getTomeDao().add(tome);;
-		
-		UserDTO registrator = new UserDTO();
-		registrator.setFirstName("FirstName");
-	
-		ResourceTypeDTO resourceTypeDTO = new ResourceTypeDTO();
-		resourceTypeDTO.setTypeName("радіочастота");
-		
-		
-		List<PointAreaDTO> PointAreaDTOs = new ArrayList<PointAreaDTO>();
-		PointAreaDTOs.add(new PointAreaDTO(1, 49, 51, 49.62, 24, 01, 31.19));
-		PointAreaDTOs.add(new PointAreaDTO(2, 49, 51, 50.01, 24, 01, 33.27));
-		PointAreaDTOs.add(new PointAreaDTO(3, 49, 51, 49.40, 24, 01, 33.56));
-		PointAreaDTOs.add(new PointAreaDTO(4, 49, 51, 49.03, 24, 01, 31.48));
-		
-		PoligonAreaDTO poligonAreaDTO = new PoligonAreaDTO();
-		poligonAreaDTO.setPoints(PointAreaDTOs);
-		ResourceAreaDTO resourceAreaDTO = new ResourceAreaDTO();
-		List<PoligonAreaDTO> poligonAreaDTOs = new ArrayList<PoligonAreaDTO>();
-		poligonAreaDTOs.add(poligonAreaDTO);
-		resourceAreaDTO.setPoligons(poligonAreaDTOs);
-		
-		List<ResourceLinearDTO> resourceLinearDTOs = new ArrayList<ResourceLinearDTO>();
-		LinearParameterDTO linearParameterDTO = new LinearParameterDTO();
-		linearParameterDTO.setDescription("cмуга радіочастот");		
-		List<SegmentLinearDTO> segments = new ArrayList<SegmentLinearDTO>() {{
-		    add(new SegmentLinearDTO(2400,2483.5));
-		    add(new SegmentLinearDTO(5150,5350));
-		    add(new SegmentLinearDTO(2500,2700));
-		}};
-		
-		ResourceLinearDTO resourceLinearDTO = new ResourceLinearDTO();
-		resourceLinearDTO.setLinearParameterDTO(linearParameterDTO);
-		resourceLinearDTO.setSegments(segments);
-		resourceLinearDTOs.add(resourceLinearDTO);
-		
-		List<ResourceDiscreteDTO> resourceDiscreteDTOs = new ArrayList<ResourceDiscreteDTO>();
-		
-		DiscreteParameterDTO disDTO1 = new DiscreteParameterDTO();
-		disDTO1.setDescription("потужність");
-		List<Double> values1 = new ArrayList<Double>() {{
-		    add(100d);
-		    add(500.55);
-		    add(23.54);
-		}};
-		ResourceDiscreteDTO resourceDiscreteDTO1 = new ResourceDiscreteDTO();
-		resourceDiscreteDTO1.setDiscreteParameterDTO(disDTO1);
-		resourceDiscreteDTO1.setValues(values1);
-		
-		DiscreteParameterDTO disDTO2 = new DiscreteParameterDTO();
-		disDTO2.setDescription("напруженість");
-		List<Double> values2 = new ArrayList<Double>() {{
-		    add(200d);
-		}};
-		ResourceDiscreteDTO resourceDiscreteDTO2 = new ResourceDiscreteDTO();
-		resourceDiscreteDTO2.setDiscreteParameterDTO(disDTO2);
-		resourceDiscreteDTO2.setValues(values2);
-		
-		
-		resourceDiscreteDTOs.add(resourceDiscreteDTO1);
-		resourceDiscreteDTOs.add(resourceDiscreteDTO2);
-		
-		ResourceDTO resourceDTO = new ResourceDTO();
-		resourceDTO.setResourceType(resourceTypeDTO);
-		resourceDTO.setIdentifier("identifier");
-		resourceDTO.setRegistratorName("FirstName");
-		resourceDTO.setTomeIdentifier("12345");
-		resourceDTO.setDate(new Date());
-		resourceDTO.setStatus(ResourceStatus.ACTIVE);
-		resourceDTO.setReasonInclusion("Паспорт громадянина україни...");
-		resourceDTO.setResourceArea(resourceAreaDTO);
-		resourceDTO.setResourceDiscrete(resourceDiscreteDTOs);
-		resourceDTO.setResourceLinear(resourceLinearDTOs);
-			
-		RegistratorService registratorService = new RegistratorServiceImpl();
-		registratorService.addResource(resourceDTO);
+//		Tome tome = new Tome();
+//		tome.setIdentifier("12345");
+//		tome.setName("name??");
+//		tome.setUser(DaoFactory.get().getUserDao().findById(1));
+//		DaoFactory.get().getTomeDao().add(tome);;
+//		
+//		UserDTO registrator = new UserDTO();
+//		registrator.setFirstName("FirstName");
+//	
+//		ResourceTypeDTO resourceTypeDTO = new ResourceTypeDTO();
+//		resourceTypeDTO.setTypeName("радіочастота");
+//		
+//		
+//		List<PointAreaDTO> PointAreaDTOs = new ArrayList<PointAreaDTO>();
+//		PointAreaDTOs.add(new PointAreaDTO(1, 49, 51, 49.62, 24, 01, 31.19));
+//		PointAreaDTOs.add(new PointAreaDTO(2, 49, 51, 50.01, 24, 01, 33.27));
+//		PointAreaDTOs.add(new PointAreaDTO(3, 49, 51, 49.40, 24, 01, 33.56));
+//		PointAreaDTOs.add(new PointAreaDTO(4, 49, 51, 49.03, 24, 01, 31.48));
+//		
+//		PoligonAreaDTO poligonAreaDTO = new PoligonAreaDTO();
+//		poligonAreaDTO.setPoints(PointAreaDTOs);
+//		ResourceAreaDTO resourceAreaDTO = new ResourceAreaDTO();
+//		List<PoligonAreaDTO> poligonAreaDTOs = new ArrayList<PoligonAreaDTO>();
+//		poligonAreaDTOs.add(poligonAreaDTO);
+//		resourceAreaDTO.setPoligons(poligonAreaDTOs);
+//		
+//		List<ResourceLinearDTO> resourceLinearDTOs = new ArrayList<ResourceLinearDTO>();
+//		LinearParameterDTO linearParameterDTO = new LinearParameterDTO();
+//		linearParameterDTO.setDescription("cмуга радіочастот");		
+//		List<SegmentLinearDTO> segments = new ArrayList<SegmentLinearDTO>() {{
+//		    add(new SegmentLinearDTO(2400,2483.5));
+//		    add(new SegmentLinearDTO(5150,5350));
+//		    add(new SegmentLinearDTO(2500,2700));
+//		}};
+//		
+//		ResourceLinearDTO resourceLinearDTO = new ResourceLinearDTO();
+//		resourceLinearDTO.setLinearParameterDTO(linearParameterDTO);
+//		resourceLinearDTO.setSegments(segments);
+//		resourceLinearDTOs.add(resourceLinearDTO);
+//		
+//		List<ResourceDiscreteDTO> resourceDiscreteDTOs = new ArrayList<ResourceDiscreteDTO>();
+//		
+//		DiscreteParameterDTO disDTO1 = new DiscreteParameterDTO();
+//		disDTO1.setDescription("потужність");
+//		List<Double> values1 = new ArrayList<Double>() {{
+//		    add(100d);
+//		    add(500.55);
+//		    add(23.54);
+//		}};
+//		ResourceDiscreteDTO resourceDiscreteDTO1 = new ResourceDiscreteDTO();
+//		resourceDiscreteDTO1.setDiscreteParameterDTO(disDTO1);
+//		resourceDiscreteDTO1.setValues(values1);
+//		
+//		DiscreteParameterDTO disDTO2 = new DiscreteParameterDTO();
+//		disDTO2.setDescription("напруженість");
+//		List<Double> values2 = new ArrayList<Double>() {{
+//		    add(200d);
+//		}};
+//		ResourceDiscreteDTO resourceDiscreteDTO2 = new ResourceDiscreteDTO();
+//		resourceDiscreteDTO2.setDiscreteParameterDTO(disDTO2);
+//		resourceDiscreteDTO2.setValues(values2);
+//		
+//		
+//		resourceDiscreteDTOs.add(resourceDiscreteDTO1);
+//		resourceDiscreteDTOs.add(resourceDiscreteDTO2);
+//		
+//		ResourceDTO resourceDTO = new ResourceDTO();
+//		resourceDTO.setResourceType(resourceTypeDTO);
+//		resourceDTO.setIdentifier("identifier");
+//		resourceDTO.setRegistratorName("FirstName");
+//		resourceDTO.setTomeIdentifier("12345");
+//		resourceDTO.setDate(new Date());
+//		resourceDTO.setStatus(ResourceStatus.ACTIVE);
+//		resourceDTO.setReasonInclusion("Паспорт громадянина україни...");
+//		resourceDTO.setResourceArea(resourceAreaDTO);
+//		resourceDTO.setResourceDiscrete(resourceDiscreteDTOs);
+//		resourceDTO.setResourceLinear(resourceLinearDTOs);
+//			
+//		RegistratorService registratorService = new RegistratorServiceImpl();
+//		registratorService.addResource(resourceDTO);
 		
 //		Role role = new Role("USER","description2");
 //		RoleDao  roleDao = DaoFactory.get().getRoleDao();
