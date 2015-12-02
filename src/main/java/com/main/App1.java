a9cffb7047a8df6faebcf5e7a42d8f92944e968f
Achyp14 <achyp@MacBook-Pro-3.local>
2015-12-02 18:48:01 +0200

    improvment in methods changeRole and changeStatus

--- src/main/java/com/main/App1.java
+++ src/main/java/com/main/App1.java
@@ -3,17 +3,30 @@ package com.main;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.registrator.community.dao.daofactory.DaoFactory;
+import org.registrator.community.dao.utils.HibernateUtil;
+import org.registrator.community.dto.AddressDTO;
+import org.registrator.community.dto.PassportDTO;
 import org.registrator.community.dto.UserDTO;
+import org.registrator.community.entity.Role;
 import org.registrator.community.service.implementation.AdminServiceImpl;
+import org.registrator.community.service.implementation.UserServiceImpl;
 import org.registrator.community.service.interfaces.AdminService;
+import org.registrator.community.service.interfaces.UserService;
+
 
 public class App1 {
-	
+
 	public static void main(String[] args) {
-		AdminService admin = new AdminServiceImpl();
-		List<UserDTO> userDtoList = new ArrayList<UserDTO>();
-		userDtoList = admin.getAllUsers();
-		admin.showAllUsers(userDtoList);
 		
+		AdminService adminService = new AdminServiceImpl();
+		List<UserDTO> userDtoList = new ArrayList<UserDTO>();
+		userDtoList = adminService.getAllUsers();
+		adminService.showAllUsers(userDtoList);
+		userDtoList.get(0).setStatus(adminService.changeUserStatus("login"));
+		userDtoList.get(0).setRole(adminService.changeRole("login"));
+		adminService.showAllUsers(userDtoList);
 	}
 }
--- src/main/java/org/registrator/community/application/Demo1.java
+++ src/main/java/org/registrator/community/application/Demo1.java
@@ -6,11 +6,9 @@ public class Demo1 {
 	
 	public static void main(String[] args) {
 		
-		
 		// Creating tables and filling in it
 		CreateTableInDB create = new CreateTableInDB();
 		create.createTables(); 
-		
 		
 		
 	}
