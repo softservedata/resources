a9cae0d93a4f07bb13c5b000925ba76ee95b0c1f
Andrii Palyha <andrew.palyga@gmail.com>
2015-12-08 16:19:13 +0200

    delete all unnecessary files

--- src/main/java/org/registrator/community/controller/MainPageController.java
+++ src/main/java/org/registrator/community/controller/MainPageController.java
@@ -1,33 +1,15 @@
 package org.registrator.community.controller;
 
-
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class MainPageController {
-	
-	@Autowired
-	private UserTestService testService;
-
-	//private TestService testService;
-	
-	@RequestMapping(value="/aaa",method=RequestMethod.GET)
-	public String getOtherPage(){
-		return "index";
-	}
 
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String getMainPage(){
 		return "index";
 	}
-
-
-
 }
 
--- src/main/java/org/registrator/community/dao/interfaces/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package org.registrator.community.dao.interfaces;
-
-import java.util.List;
-
-import org.registrator.community.entity.User;
-
-public interface IUserDao {
-	public List<User> getUsersListByPortions(int from, int quantity);
-}
