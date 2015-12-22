6caee3a33ba4f958a444db4fd5233fd6d78e6061
korolyuk92 <korolyuk92@gmail.com>
2015-12-22 14:05:28 +0200

    fix method delete in resource type, change properties and controllers

--- src/main/java/org/registrator/community/service/ResourceTypeService.java
+++ src/main/java/org/registrator/community/service/ResourceTypeService.java
@@ -11,9 +11,7 @@ public interface ResourceTypeService{
 	ResourceType addResourceType (ResourceType resourceType);
 	void delete(ResourceType resourceType);
 	ResourceType findByName(String name);
-//	public ResourceType editResourceType(ResourceType resourceType);
 	List<ResourceType> findAll();
-	/*ResourceType editResourceType(String typeName);*/
 	ResourceTypeDTO editResourceType(ResourceTypeDTO resourceTypeDTO);
 	ResourceType findById(Integer id);
 	ResourceTypeDTO addResourceTypeDTO(ResourceTypeDTO resourceTypeDTO);
