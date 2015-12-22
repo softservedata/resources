004fb051d559006a31c2e779b22bacf8dcc2fb8d
Andrii Palyha <andrew.palyga@gmail.com>
2015-12-22 16:14:29 +0200

    add DataTable DTO, change UserRepository

--- src/main/java/org/registrator/community/dao/UserRepository.java
+++ src/main/java/org/registrator/community/dao/UserRepository.java
@@ -6,11 +6,12 @@ import org.registrator.community.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User,String>, JpaSpecificationExecutor<User>{
 
 	public Page<User> findAll(Pageable pageable);
 	
