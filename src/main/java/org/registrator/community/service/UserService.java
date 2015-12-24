c3ab56455ea3c607750c49eb89b4ec1b664349cc
Achyp14 <achyp@MacBook-Pro-4.local>
2015-12-24 18:05:54 +0200

    merge

--- src/main/java/org/registrator/community/service/UserService.java
+++ src/main/java/org/registrator/community/service/UserService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.registrator.community.service;
 
 import java.util.List;
@@ -30,35 +29,3 @@ public interface UserService {
 
 }
 
-=======
-package org.registrator.community.service;
-
-import java.util.List;
-
-import org.registrator.community.dto.UserDTO;
-import org.registrator.community.entity.Role;
-import org.registrator.community.entity.User;
-import org.registrator.community.enumeration.UserStatus;
-
-public interface UserService {
-
-	public void changeUserStatus(String login, UserStatus userStatus);
-
-	public List<UserDTO> getAllRegistratedUsers();
-	
-	public User getUserByLogin(String login);
-
-	public void changeUserRole(String login, Role role);
-	
-	public void editUserInformation(UserDTO userDto);
-	
-	public List<UserStatus> fillInUserStatus();
-	
-	public List<UserDTO> getUserDtoList();
-	
-	public UserDTO getUserDto(String login);
-	
-	public List<UserDTO> getAllInactiveUsers();// set Role of inactive user to "USER"
-}
-
->>>>>>> 4199a4e98095fc6cbf8e72f3c87067aed63b456e
