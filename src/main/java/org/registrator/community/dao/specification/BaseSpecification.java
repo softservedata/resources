ffdf6c7e250c9c4bb107c1fbfe1807f72ee16963
Andrii Palyha <andrew.palyga@gmail.com>
2016-01-12 19:20:02 +0200

    change search specification

--- src/main/java/org/registrator/community/dao/specification/BaseSpecification.java
+++ src/main/java/org/registrator/community/dao/specification/BaseSpecification.java
@@ -2,14 +2,14 @@ package org.registrator.community.dao.specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.registrator.community.dto.search.SearchColumn;
-import org.registrator.community.entity.User;
 import org.springframework.data.jpa.domain.Specification;
 
-public class UserSpecification<User> implements Specification<User>{
+public class BaseSpecification<T> implements Specification<T>{
 	
 	private SearchColumn criteria;
 	
@@ -22,7 +22,7 @@ public class UserSpecification<User> implements Specification<User>{
 	}
 
 	@Override
-	public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query,
+	public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query,
 			CriteriaBuilder builder) {
 		if (criteria.getSearch().getCompareSign().equalsIgnoreCase("greater")) {
             return builder.greaterThanOrEqualTo(
@@ -32,18 +32,25 @@ public class UserSpecification<User> implements Specification<User>{
             return builder.lessThanOrEqualTo(
               root.<String> get(criteria.getData()), criteria.getSearch().getValue());
         } 
-        else if (criteria.getSearch().getCompareSign().equalsIgnoreCase(":")) {
+        else if (criteria.getSearch().getCompareSign().equalsIgnoreCase("like")) {
             if (root.get(criteria.getData()).getJavaType() == String.class) {
                 return builder.like(
                   root.<String>get(criteria.getName()), "%" + criteria.getSearch().getValue() + "%");
-            } else {
-                return builder.equal(root.get(criteria.getData()), criteria.getSearch().getValue());
             }
+            return builder.lessThanOrEqualTo(getPathFromRoot( root)
+                    , criteria.getSearch().getValue());
         }
         else if (criteria.getSearch().getCompareSign().equalsIgnoreCase("equal")){
-        	return builder.equal(root.get(criteria.getData()), criteria.getSearch().getValue());
+        	return builder.equal(getPathFromRoot(root), criteria.getSearch().getValue());
         }
         return null;
 	}
-
+	
+	public Path<String> getPathFromRoot(Root<T> root){
+		if(criteria.getData().contains("_")){
+			String[]pathParameters = criteria.getData().split("_");
+			return root.join(pathParameters[0]).get(pathParameters[1]);
+		}
+		return root.get(criteria.getData());
+	}
 }
