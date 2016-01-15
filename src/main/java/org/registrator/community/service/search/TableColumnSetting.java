3cd8c3048840f4a184cd87dd3b24d22df9e800b3
Andrii Palyha <andrew.palyga@gmail.com>
2016-01-15 12:38:53 +0200

    add tableSearch XMLConfiguration parser

--- src/main/java/org/registrator/community/service/search/TableColumnSetting.java
+++ src/main/java/org/registrator/community/service/search/TableColumnSetting.java
@@ -2,14 +2,27 @@ package org.registrator.community.service.search;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class TableColumnSetting {
 	
+	@XmlElement(required=false,defaultValue="search")
 	private String type;
 	
+	@XmlElement(required=false)
 	private String title;
 	
+	@XmlElement(required=false)
 	private String data;
 	
+	@XmlElementWrapper(name = "searchOperation")
+	@XmlElement(name="operationType")
 	private List<String> searchType;
 
 	public String getTitle() {
@@ -43,5 +56,4 @@ public class TableColumnSetting {
 	public void setSearchType(List<String> searchType) {
 		this.searchType = searchType;
 	}
-	
 }
