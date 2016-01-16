cabd60a493ea8f0a2f3a4ac37e36f0771fa3d8bd
Andrii Palyha <andrew.palyga@gmail.com>
2016-01-16 15:31:06 +0200

    add testing profile to configuration, do refactoring in config packege

---
+++ src/main/java/org/registrator/community/config/root/SpringRootConfig.java
@@ -0,0 +1,27 @@
+package org.registrator.community.config.root;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@ComponentScan({"org.registrator.community.dto",
+	"org.registrator.community.service"})
+@EnableTransactionManagement
+public class SpringRootConfig {
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory,
+                                                         DriverManagerDataSource dataSource) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
+    }
+}
---
+++ src/main/java/org/registrator/community/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,60 @@
+package org.registrator.community.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.registrator.community.dao.UserRepository;
+import org.registrator.community.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsService")
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	UserService userService;
+
+	private static final Logger logger = LoggerFactory.getLogger(UserDetailServiceImpl.class);
+	
+	@Override
+	public UserDetails loadUserByUsername(final String login)
+			throws UsernameNotFoundException {
+
+		org.registrator.community.entity.User userEntity = userRepository.findUserByLogin(login);
+
+		if (userEntity == null){
+			logger.error("User - {} - not found", userEntity.getLogin());
+			throw new UsernameNotFoundException("Помилка в паролі, чи емейлі");
+		}
+		else{
+			logger.info("Requested user - {} - is found: ", userEntity.getLogin());
+		}
+
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_"+ userEntity.getRole()));
+
+		return buildUserForAuthentication(userEntity,authorities);
+	}
+	
+	
+	private User buildUserForAuthentication(org.registrator.community.entity.User userEntity, List<GrantedAuthority> authorities) {
+		//return new User(userEntity.getFirstName()+"  "+userEntity.getLastName(), userEntity.getPassword(), true, true, true, true, authorities);
+		
+		//userService.login(userEntity.getLogin(), userEntity.getPassword());
+		//return new User(userEntity.getLogin(), userRepository.getUsersPasswordHash(userEntity.getPassword()), authorities);
+		return new User(userEntity.getLogin(), userEntity.getPassword(), authorities);
+	}
+		
+	
+}
