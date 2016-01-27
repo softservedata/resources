f58303c821fccefc8d04e7bd84b020bc89406130
just-olya <e7jardin@gmail.com>
2016-01-27 14:13:59 +0200

    Adjusted Registration form to general application UI style

--- src/main/java/org/registrator/community/service/impl/UserServiceImpl.java
+++ src/main/java/org/registrator/community/service/impl/UserServiceImpl.java
@@ -378,16 +378,21 @@ public class UserServiceImpl implements UserService {
 			passport.setPublishedByData(registrationForm.getPublishedByData());
 
 			passportRepository.saveAndFlush(passport);
-			log.info("Inserted passport data for user '{0}', passport_data_id = ", user.getLogin(), passport.getPassportId());
+			log.info("Inserted passport data for user {0}, passport_data_id = {1}", user.getLogin(), passport.getPassportId());
 
 			// insert user's address records into "address" table
+			Address address = new Address();
+			address.setUser(user);
+			address.setCity(registrationForm.getCity());
+			address.setRegion(registrationForm.getRegion());
+			address.setDistrict(registrationForm.getDistrict());
+			address.setStreet(registrationForm.getStreet());
+			address.setBuilding(registrationForm.getBuilding());
+			address.setFlat(registrationForm.getFlat());
+			address.setPostCode(registrationForm.getPostcode());
 
-//			Address address = new Address();
-//			//
-//			address.setUser(user);
-//			address.setBuilding();
-//			addressRepository.saveAndFlush(address);
-
+			addressRepository.saveAndFlush(address);
+			log.info("Inserted address data for user {0}, address_id = {1}", user.getLogin(), address.getAddressId());
 		}
 		
 	}
@@ -503,17 +508,4 @@ public class UserServiceImpl implements UserService {
        
         return userdto;
     }
-	
-    
-    
-    // @Override
-	// public boolean recoverUsersPassword(String email, String
-	// usersCaptchaAnswer, String captchaFileName) {
-	// if(validateUsersEmail(email) && validateCaptchaCode(captchaFileName)){
-	// return true;
-	// }
-	// return false; //- "There is no such email in the DB" or "Entered captcha
-	// code is incorrect"
-	// }
-
 }
