1cdd368db2b11d6b045f94a1cbeb9cdea2584771
korolyuk92 <korolyuk92@gmail.com>
2016-02-01 17:46:48 +0200

    changed TestDataInitializer

--- src/main/java/org/registrator/community/init/TestDataInitializer.java
+++ src/main/java/org/registrator/community/init/TestDataInitializer.java
@@ -11,6 +11,7 @@ import org.registrator.community.entity.Inquiry;
 import org.registrator.community.entity.Resource;
 import org.registrator.community.entity.ResourceType;
 import org.registrator.community.entity.Role;
+import org.registrator.community.entity.TerritorialCommunity;
 import org.registrator.community.entity.Tome;
 import org.registrator.community.entity.User;
 import org.registrator.community.enumeration.RoleType;
@@ -28,6 +29,14 @@ public class TestDataInitializer {
 		SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
 		Session session = sessionFactory.openSession();
 		
+		Transaction territorialCommunity = session.beginTransaction();
+        TerritorialCommunity globalTerritorialCommunity = new TerritorialCommunity();
+        globalTerritorialCommunity.setName("Україна");
+        session.persist(globalTerritorialCommunity);
+        territorialCommunity.commit();
+		
+		
+		
         Transaction roleTransaction = session.beginTransaction();
         
         Role roleUser = new Role(RoleType.USER,"description");
@@ -39,20 +48,31 @@ public class TestDataInitializer {
         Role roleRegistrator = new Role(RoleType.REGISTRATOR,"description");
         session.persist(roleRegistrator);
         
+        Role roleCommissioner = new Role(RoleType.COMMISSIONER,"description");
+        session.persist(roleCommissioner);
+        
         roleTransaction.commit();
         Transaction userTransaction = session.beginTransaction();
         
         User user = new User("user","$2a$10$Wcuw6mLD18wVT5diGYncJeVyL8J1bTSIly2IbLUX2bJ.UWZPC.qS.",
         		roleUser,"Іван","Головатий","Сергійович","ivan@gmail.com","UNBLOCK");
+        user.setDateOfAccession(new Date());
+        user.setTerritorialCommunity(globalTerritorialCommunity);
+        
         session.persist(user);
         
         User admin = new User("admin","$2a$10$tkROwYPOXyBmKjarHW1rbuOOez2Z5gfkFCbUXUbOv1OY2wgekbZNC",
         		roleAdmin,"Сергій","Головатий","Сергійович","sergey@gmail.com","UNBLOCK");
+        user.setDateOfAccession(new Date());
+        admin.setTerritorialCommunity(globalTerritorialCommunity);
         session.persist(admin);
         
         User registrator = new User("registrator","$2a$10$KJdq1wmP3MctLh.lEdAuseUCnSRdhJo8S7qwaZHFEUoGhfjOsOnrm",
         		roleRegistrator,"Євген","Михалкевич","Сергійович","evgen@gmail.com","UNBLOCK");
+        user.setDateOfAccession(new Date());
+        registrator.setTerritorialCommunity(globalTerritorialCommunity);
         session.persist(registrator);
+        
         
         userTransaction.commit();
         Transaction resourceTypeTransaction = session.beginTransaction();
