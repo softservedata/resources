6c0a82ff9a50f067caef0601dfaedb0f69699065
okirkach <aleksei.kirkach@gmail.com>
2016-02-07 01:44:20 +0200

    Add possibility to add more than one polygon to one resource.

--- WebContent/resource/js/showResourceMap.js
+++ WebContent/resource/js/showResourceMap.js
@@ -12,36 +12,38 @@
         };
         map = new google.maps.Map(document.getElementById("map_canvas"), mapProp);
 
-        var polygonCoords = [];
+
         var bounds = new google.maps.LatLngBounds();
 
-        $(".coordinatesPoint").each(function () {
-            var latitudeDegrees = Number($(this).find(".latitudeDegrees").html());
-            var latitudeMinutes = Number($(this).find(".latitudeMinutes").html());
-            var latitudeSeconds = Number($(this).find(".latitudeSeconds").html());
-            var longitudeDegrees = Number($(this).find(".longitudeDegrees").html());
-            var longitudeMinutes = Number($(this).find(".longitudeMinutes").html());
-            var longitudeSeconds = Number($(this).find(".longitudeSeconds").html());
+        $(".polygon").each(function () {
+            var polygonCoords = [];
 
-            var lat = latitudeDegrees + latitudeMinutes / 60 + latitudeSeconds / 3600;
-            var lng = longitudeDegrees + longitudeMinutes / 60 + longitudeSeconds / 3600;
+            $(this).find(".coordinatesPoint").each(function () {
+                var latitudeDegrees = Number($(this).find(".latitudeDegrees").html());
+                var latitudeMinutes = Number($(this).find(".latitudeMinutes").html());
+                var latitudeSeconds = Number($(this).find(".latitudeSeconds").html());
+                var longitudeDegrees = Number($(this).find(".longitudeDegrees").html());
+                var longitudeMinutes = Number($(this).find(".longitudeMinutes").html());
+                var longitudeSeconds = Number($(this).find(".longitudeSeconds").html());
 
-            var latLng = new google.maps.LatLng(lat, lng);
-            bounds.extend(latLng);
-            polygonCoords.push(latLng);
+                var lat = latitudeDegrees + latitudeMinutes / 60 + latitudeSeconds / 3600;
+                var lng = longitudeDegrees + longitudeMinutes / 60 + longitudeSeconds / 3600;
+
+                var latLng = new google.maps.LatLng(lat, lng);
+                bounds.extend(latLng);
+                polygonCoords.push(latLng);
+            });
+
+            var polygon = new google.maps.Polygon({
+                path: polygonCoords, // Координаты
+                strokeColor: "#FF0000", // Цвет обводки
+                strokeOpacity: 0.8, // Прозрачность обводки
+                strokeWeight: 2, // Ширина обводки
+                fillColor: "#0000FF", // Цвет заливки
+                fillOpacity: 0.4 // Прозрачность заливки
+            });
+            polygon.setMap(map);
         });
-        polygonCoords.push(new google.maps.LatLng(centerlat, centerlng));
-
-        var polygon = new google.maps.Polygon({
-            path: polygonCoords, // Координаты
-            strokeColor: "#FF0000", // Цвет обводки
-            strokeOpacity: 0.8, // Прозрачность обводки
-            strokeWeight: 2, // Ширина обводки
-            fillColor: "#0000FF", // Цвет заливки
-            fillOpacity: 0.4 // Прозрачность заливки
-        });
-
-        polygon.setMap(map);
         map.fitBounds(bounds);
     }
 
@@ -49,10 +51,14 @@
        if($(this).hasClass("active")) {
            $(this).removeClass("active");
            $(".coordinates").hide();
+           $(this).find(".glyphicon-triangle-right").show();
+           $(this).find(".glyphicon-triangle-bottom").hide();
        }
        else {
            $(this).addClass("active");
            $(".coordinates").show();
+           $(this).find(".glyphicon-triangle-right").hide();
+           $(this).find(".glyphicon-triangle-bottom").show();
        }
     });
 
