7b23c900fef84758a4c436000f5a53be3624c955
Achyp14 <achyp@MacBook-Pro-4.local>
2016-02-07 22:47:30 +0200

    fix status

--- WebContent/resource/js/changeStatus.js
+++ WebContent/resource/js/changeStatus.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).on('change', '#userStatusId', function(event) {
 	var json = {
 		"status" : $("#userStatusId").val(),
@@ -22,28 +21,3 @@ $(document).on('change', '#userStatusId', function(event) {
 
 	});
 });
-=======
-$(document).on('change', '#userStatusId', function(event) {
-	var json = {
-		"status" : $("#userStatusId").val(),
-		"login" : $("#login").text()
-	};
-
-	$.ajax({
-		type : "POST",
-		url : "get-all-inactive-users",
-		dataType : "text",
-		data : JSON.stringify(json),
-		contentType : 'application/json; charset=utf-8',
-
-		success : function() {
-			var tr = $(event.target).closest("tr");
-			tr.css("background-color", "#000000");
-			tr.fadeIn(1000).fadeOut(200, function() {
-				tr.remove();
-			})
-		}
-
-	});
-});
->>>>>>> 0bc10303861f3d1e44cca7f2cb34cb02d4101af7
