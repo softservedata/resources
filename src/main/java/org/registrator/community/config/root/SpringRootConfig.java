7bdc5d1a13ae25d76c108b86f2b3c29f301eba1c
vitaliH <vitaliihorban93@gmail.com>
2016-02-08 16:49:59 +0200

    add bruteforce protection

--- src/main/java/org/registrator/community/config/root/SpringRootConfig.java
+++ src/main/java/org/registrator/community/config/root/SpringRootConfig.java
@@ -1,12 +1,14 @@
 package org.registrator.community.config.root;
 
 import org.registrator.community.config.AdditionalAppConfig;
+import org.registrator.community.service.impl.CronService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,6 +17,7 @@ import javax.persistence.EntityManagerFactory;
 @Configuration
 @ComponentScan({"org.registrator.community.dto",
 	"org.registrator.community.service","org.registrator.community.validator"})
+@EnableScheduling
 @EnableTransactionManagement
 @Import(AdditionalAppConfig.class)
 public class SpringRootConfig {
@@ -27,4 +30,10 @@ public class SpringRootConfig {
         transactionManager.setDataSource(dataSource);
         return transactionManager;
     }
+    
+    @Bean
+    public CronService bean() {
+        return new CronService();
+    }
+    
 }
