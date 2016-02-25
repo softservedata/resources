5d9b974bfc58b836bafa44b2762c6b6658a608b6
okirkach <aleksei.kirkach@gmail.com>
2016-02-25 23:11:29 +0200

    Fix LazyInitializationException for ResourceServiceTest

--- src/test/java/org/registrator/community/service/ResourceServiceTest.java
+++ src/test/java/org/registrator/community/service/ResourceServiceTest.java
@@ -13,11 +13,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import org.junit.Assert;
 
+import javax.transaction.Transactional;
+
 /**
  * Unit test for Resource service methods
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("testing")
+@Transactional
 @ContextConfiguration(classes={TestingConfiguration.class,LoggingConfig.class,SpringRootConfig.class})
 public class ResourceServiceTest {
 
