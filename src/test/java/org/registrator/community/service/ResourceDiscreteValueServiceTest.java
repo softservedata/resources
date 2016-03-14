ab904f90a1baf77dc5fb5864adef49eb4eaf87ee
Roman Golyuk <roman.golyuk@gmail.com>
2016-03-14 17:03:05 +0200

    Completed unit tests for ResourceService, ResourceLinearValueService, ResourceDiscreteValueService

--- src/test/java/org/registrator/community/service/ResourceDiscreteValueServiceTest.java
+++ src/test/java/org/registrator/community/service/ResourceDiscreteValueServiceTest.java
@@ -1,52 +1,45 @@
 package org.registrator.community.service;
 
-import static java.util.Arrays.asList;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import javax.transaction.Transactional;
-
 import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.registrator.community.config.root.SpringRootConfig;
-import org.registrator.community.config.root.TestingConfiguration;
+import org.powermock.api.support.membermodification.MemberModifier;
 import org.registrator.community.dao.DiscreteParameterRepository;
 import org.registrator.community.dao.ResourceDiscreteValueRepository;
 import org.registrator.community.entity.DiscreteParameter;
 import org.registrator.community.entity.Resource;
+import org.registrator.community.entity.ResourceDiscreteValue;
+import org.registrator.community.entity.ResourceLinearValue;
 import org.registrator.community.service.impl.ResourceDiscreteValueServiceImpl;
 import org.slf4j.Logger;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.assertEquals;
+
 /**
- * Created by roman.golyuk on 02.03.2016.
  * Test class for ResourceDiscreteValueService interface implementation
  * */
 
-@ActiveProfiles("testing")
-@Transactional
-@ContextConfiguration(classes={TestingConfiguration.class,SpringRootConfig.class})
 public class ResourceDiscreteValueServiceTest {
 
     @InjectMocks
-    private ResourceDiscreteValueService serviceUnderTest;
-
-    @Mock
-    private Resource resource;
+    private ResourceDiscreteValueService valueService;
 
     @Mock
     private DiscreteParameter parameter;
-
-    private Integer idParameter;
 
     @Mock(answer = Answers.RETURNS_SMART_NULLS)
     private ResourceDiscreteValueRepository valueRepository;
@@ -54,115 +47,237 @@ public class ResourceDiscreteValueServiceTest {
     @Mock(answer = Answers.RETURNS_SMART_NULLS)
     private DiscreteParameterRepository parameterRepository;
 
-    @Mock
+    private static final String BEGIN = "begin test";
+    private static final String END = "end test";
+    private static final double VALUE_SEARCH = 47.0;
+    private static final Integer ID_PARAMETER = 47;
+
     private Logger logger;
+    private ResourceDiscreteValue resourceValue;
+    private String resourceIdentifier = "test";
+    private Resource resource;
 
     @BeforeMethod
     public void initMocks() throws Exception {
-        serviceUnderTest = new ResourceDiscreteValueServiceImpl();
+        valueService = new ResourceDiscreteValueServiceImpl();
         MockitoAnnotations.initMocks(this);
 
-        idParameter = 47;
-        when(parameterRepository.findByDiscreteParameterId(idParameter)).thenReturn(parameter);
+        logger = LoggerFactory.getLogger("");
+        MemberModifier
+                .field(ResourceDiscreteValueServiceImpl.class, "logger")
+                .set(valueService, logger);
+
+        when(parameterRepository.findByDiscreteParameterId(ID_PARAMETER)).thenReturn(parameter);
+
+        resourceValue = mock(ResourceDiscreteValue.class);
+        resource = mock(Resource.class);
+        when(resourceValue.getResource()).thenReturn(resource);
+        when(resource.getIdentifier()).thenReturn(resourceIdentifier);
+        when(resourceValue.getResource()).thenReturn(resource);
 
     }
 
 
     @Test
     public void findByResource() throws Exception {
-        serviceUnderTest.findByResource(resource);
+        logger.debug(BEGIN);
+        List<ResourceDiscreteValue> expected = asList(resourceValue);
+        when(valueRepository.findByResource(resource)).thenReturn(expected);
+
+        List<ResourceDiscreteValue> actual = valueService.findByResource(resource);
 
         verify(valueRepository, times(1)).findByResource(resource);
+        assertEquals(actual, expected);
+        logger.debug(END);
     }
+
 
     @Test
     public void findAllByDiscreteParameter() throws Exception {
-        serviceUnderTest.findAllByDiscreteParameter(parameter);
+        logger.debug(BEGIN);
+        List<ResourceDiscreteValue> expected = asList(resourceValue);
+        when(valueRepository.findAllByDiscreteParameter(parameter)).thenReturn(expected);
+
+        List<ResourceDiscreteValue> actual = valueService.findAllByDiscreteParameter(parameter);
 
         verify(valueRepository, times(1)).findAllByDiscreteParameter(parameter);
+        assertEquals(actual, expected);
+        logger.debug(END);
     }
 
     @Test
     public void findAllByValueAndDiscreteParameter() throws Exception {
-        double value = 47.0;
+        logger.debug(BEGIN);
+        List<ResourceDiscreteValue> expected =  asList(resourceValue);
+        when(valueRepository.findAllByValueAndDiscreteParameter(VALUE_SEARCH, parameter)).thenReturn(expected);
 
-        serviceUnderTest.findAllByValueAndDiscreteParameter(value, parameter);
-        verify(valueRepository, times(1)).findAllByValueAndDiscreteParameter(value, parameter);
+        List<ResourceDiscreteValue> actual =
+                valueService.findAllByValueAndDiscreteParameter(VALUE_SEARCH, parameter);
+        verify(valueRepository, times(1)).findAllByValueAndDiscreteParameter(VALUE_SEARCH, parameter);
+        assertEquals(actual, expected);
+        logger.debug(END);
     }
 
     @Test
     public void findAllByBiggerValueAndDiscreteParameter() throws Exception {
-        double value = 47.0;
+        logger.debug(BEGIN);
+        List<ResourceDiscreteValue> expected =  asList(resourceValue);
+        when(valueRepository.findAllByBiggerValueAndDiscreteParameter(VALUE_SEARCH, parameter)).thenReturn(expected);
 
-        serviceUnderTest.findAllByBiggerValueAndDiscreteParameter(value, parameter);
-        verify(valueRepository, times(1)).findAllByBiggerValueAndDiscreteParameter(value, parameter);
+        List<ResourceDiscreteValue> actual =
+                valueService.findAllByBiggerValueAndDiscreteParameter(VALUE_SEARCH, parameter);
+        verify(valueRepository, times(1)).findAllByBiggerValueAndDiscreteParameter(VALUE_SEARCH, parameter);
+        assertEquals(actual, expected);
+        logger.debug(END);
     }
 
     @Test
     public void findAllBySmallerValueAndDiscreteParameter() throws Exception {
-        double value = 47.0;
+        logger.debug(BEGIN);
 
-        serviceUnderTest.findAllBySmallerValueAndDiscreteParameter(value, parameter);
-        verify(valueRepository, times(1)).findAllBySmallerValueAndDiscreteParameter(value, parameter);
+        List<ResourceDiscreteValue> expected =  asList(resourceValue);
+        when(valueRepository.findAllBySmallerValueAndDiscreteParameter(VALUE_SEARCH, parameter)).thenReturn(expected);
+
+        List<ResourceDiscreteValue> actual =
+                valueService.findAllBySmallerValueAndDiscreteParameter(VALUE_SEARCH, parameter);
+        verify(valueRepository, times(1)).findAllBySmallerValueAndDiscreteParameter(VALUE_SEARCH, parameter);
+        assertEquals(actual, expected);
+        logger.debug(END);
     }
+
 
     @Test
     public void findResourcesbyDiscreteParamLess() throws Exception {
-        serviceUnderTest = spy(serviceUnderTest);
+        logger.debug(BEGIN);
 
+        List<ResourceDiscreteValue> resourceDiscreteValues =  asList(resourceValue);
+        when(valueRepository.findAllBySmallerValueAndDiscreteParameter(VALUE_SEARCH, parameter))
+                .thenReturn(resourceDiscreteValues);
 
-        double value = 47.0;
+        Set<String> expected = new HashSet<>();
+        expected.add(resourceIdentifier);
 
-        serviceUnderTest.findResourcesByDiscreteParam(idParameter, "less", value);
-        verify(parameterRepository, times(1)).findByDiscreteParameterId(idParameter);
-        verify(valueRepository, times(1)).findAllBySmallerValueAndDiscreteParameter(eq(value), any());
+        Set<String> actual =
+            valueService.findResourcesByDiscreteParam(ID_PARAMETER, "less", VALUE_SEARCH);
 
+        verify(parameterRepository, times(1)).findByDiscreteParameterId(ID_PARAMETER);
+        verify(valueRepository, times(1)).findAllBySmallerValueAndDiscreteParameter(eq(VALUE_SEARCH), any());
+        assertEquals(actual, expected);
+
+        logger.debug(END);
     }
 
     @Test
     public void findResourcesbyDiscreteParamGreater() throws Exception {
-        serviceUnderTest = spy(serviceUnderTest);
+        logger.debug(BEGIN);
 
+        List<ResourceDiscreteValue> resourceDiscreteValues =  asList(resourceValue);
+        when(valueRepository.findAllByBiggerValueAndDiscreteParameter(VALUE_SEARCH, parameter))
+                .thenReturn(resourceDiscreteValues);
 
-        double value = 47.0;
+        Set<String> expected = new HashSet<>();
+        expected.add(resourceIdentifier);
 
-        serviceUnderTest.findResourcesByDiscreteParam(idParameter, "greater", value);
-        verify(parameterRepository, times(1)).findByDiscreteParameterId(idParameter);
-        verify(valueRepository, times(1)).findAllByBiggerValueAndDiscreteParameter(eq(value), any());
+        Set<String> actual =
+                valueService.findResourcesByDiscreteParam(ID_PARAMETER, "greater", VALUE_SEARCH);
 
+        verify(parameterRepository, times(1)).findByDiscreteParameterId(ID_PARAMETER);
+        verify(valueRepository, times(1)).findAllByBiggerValueAndDiscreteParameter(eq(VALUE_SEARCH), any());
+        assertEquals(actual, expected);
+
+        logger.debug(END);
     }
 
     @Test
     public void findResourcesbyDiscreteParamEqual() throws Exception {
-        serviceUnderTest = spy(serviceUnderTest);
+        logger.debug(BEGIN);
 
+        List<ResourceDiscreteValue> resourceDiscreteValues =  asList(resourceValue);
+        when(valueRepository.findAllByValueAndDiscreteParameter(VALUE_SEARCH, parameter))
+                .thenReturn(resourceDiscreteValues);
 
-        double value = 47.0;
+        Set<String> expected = new HashSet<>();
+        expected.add(resourceIdentifier);
 
-        serviceUnderTest.findResourcesByDiscreteParam(idParameter, "equal", value);
-        verify(parameterRepository, times(1)).findByDiscreteParameterId(idParameter);
-        verify(valueRepository, times(1)).findAllByValueAndDiscreteParameter(eq(value), any());
+        Set<String> actual =
+                valueService.findResourcesByDiscreteParam(ID_PARAMETER, "equal", VALUE_SEARCH);
+
+        verify(parameterRepository, times(1)).findByDiscreteParameterId(ID_PARAMETER);
+        verify(valueRepository, times(1)).findAllByValueAndDiscreteParameter(eq(VALUE_SEARCH), any());
+        assertEquals(actual, expected);
+
+        logger.debug(END);
+    }
+
+    @Test(dataProvider = "findParamsProvider")
+    public void findResourcesByParamsList(
+    		List<Integer> idList, 
+    		List<String> compareSignList, 
+    		List<Double> valuesList) throws Exception {
+    	
+        logger.debug(BEGIN);
+        List<ResourceDiscreteValue> resourceDiscreteValues =  asList(resourceValue);
+        when(valueRepository.findAllBySmallerValueAndDiscreteParameter(VALUE_SEARCH, parameter))
+                .thenReturn(resourceDiscreteValues);
+        when(valueRepository.findAllByBiggerValueAndDiscreteParameter(VALUE_SEARCH, parameter))
+                .thenReturn(resourceDiscreteValues);
+        when(valueRepository.findAllByValueAndDiscreteParameter(VALUE_SEARCH, parameter))
+                .thenReturn(resourceDiscreteValues);
+
+        Set<String> expected = new HashSet<>();
+        expected.add(resourceIdentifier);
+
+        Set<String> actual = valueService.findResourcesByParamsList(idList, compareSignList, valuesList);
+        
+        for (int i = 0; i < compareSignList.size(); i++) {
+        	String compareSign = compareSignList.get(i);
+        	if (valuesList.get(i) != null) {
+	        	switch (compareSign) {
+	        	case "equal":
+	        		verify(valueRepository, atLeast(1)).findAllByValueAndDiscreteParameter(eq(VALUE_SEARCH), any());
+	        		break;
+	        	case "greater":
+	        		verify(valueRepository, atLeast(1)).findAllByBiggerValueAndDiscreteParameter(eq(VALUE_SEARCH), any());
+	        		break;
+	        	case "less":
+	        		verify(valueRepository, atLeast(1)).findAllBySmallerValueAndDiscreteParameter(eq(VALUE_SEARCH), any());
+	        		break;
+	        	}
+        	}
+        }
+        
+        assertEquals(actual, expected);
 
     }
 
-
-    @Test
-    public void findResourcesByParamsList() throws Exception {
-        serviceUnderTest = spy(serviceUnderTest);
-
-
-        double value = 47.0;
-
-        serviceUnderTest.findResourcesByParamsList(
-                asList(47, 47, 47),
-                asList("equal", "greater", "less"),
-                asList(value, value, value));
-        verify(parameterRepository, times(3)).findByDiscreteParameterId(idParameter);
-        verify(valueRepository, times(1)).findAllBySmallerValueAndDiscreteParameter(eq(value), any());
-        verify(valueRepository, times(1)).findAllByBiggerValueAndDiscreteParameter(eq(value), any());
-        verify(valueRepository, times(1)).findAllByValueAndDiscreteParameter(eq(value), any());
-
+    @DataProvider(name = "findParamsProvider")
+    // creates parameters for findResourcesByParamsList
+    public Object[][] findParamsProvider() {
+    	int paramsCount = 3;
+    	List<Object[]> listParams = new ArrayList<>();
+    	
+    	//normal flow of program
+    	listParams.add(new Object[]{
+	    	asList(ID_PARAMETER, ID_PARAMETER, ID_PARAMETER),
+	        asList("equal", "greater", "less"),
+	        asList(VALUE_SEARCH, VALUE_SEARCH, VALUE_SEARCH)
+    	});
+    	
+    	//null value
+    	listParams.add(new Object[]{
+	    	asList(ID_PARAMETER, ID_PARAMETER),
+	        asList("equal", "greater" ),
+	        asList(VALUE_SEARCH, null)
+    	});
+    	
+    	
+    	Object[][] result = new Object[listParams.size()][paramsCount];
+    	for (int i = 0; i < listParams.size(); i++) {
+    		for (int k = 0; k < paramsCount; k++) {
+    			result[i][k] = listParams.get(i)[k];
+    		}
+    	}
+    	return result;
     }
-
 
 }
